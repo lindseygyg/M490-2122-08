ada2a1524e6879abde5d7c69ca1fb7a3160918e2
Lindsey <l_gilles@live.concordia.ca>
2022-03-31 14:27:00 -0400

    final modifications - accounting for ZeroDivisionError in a new way since previous way was tested and failed. Editing some html/css files.

--- CPRPROJECT_Final_Django/CPR/settings.py
+++ CPRPROJECT_Final_Django/CPR/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-m8ao#*%0n%p#yw-=(wr)$4p8wlf-ff1wc=hpz&%+tws*s&0q00
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.20.10.4', 'raspberrypi', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static",
                     '/var/www/static/'
